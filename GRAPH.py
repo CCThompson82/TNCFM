6801b9c5422b27cd7969368d8d5563433185d930
Christopher Thompson <christophercthompson82@gmail.com>
2017-01-30 19:26:26 +0000

    style : Fixed typo before run

--- GRAPH.py
+++ GRAPH.py
@@ -69,27 +69,43 @@ with graph.as_default() :
 
     def nn(data, dropout = False) :
         with tf.name_scope('Convolution') :
-            c1 = tf.nn.conv2d(data, filter = W_conv1, strides = [1, stride, stride, 1], padding = 'SAME')
-            c2 = tf.nn.conv2d(c1, filter = W_conv2, strides = [1, stride, stride, 1], padding = 'SAME')
+            c1 = tf.nn.relu(tf.nn.conv2d(data, filter = W_conv1, strides = [1, stride, stride, 1], padding = 'SAME'))
+            if dropout :
+                c1 = tf.nn.dropout(c1, keep_prob_convs)
+            c2 = tf.nn.relu(tf.nn.conv2d(c1, filter = W_conv2, strides = [1, stride, stride, 1], padding = 'SAME'))
             c2_pool = tf.nn.max_pool(c2, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'VALID')
-            c3 = tf.nn.conv2d(c2_pool, filter = W_conv3, strides = [1, stride, stride, 1], padding = 'SAME')
-            c4 = tf.nn.conv2d(c3, filter = W_conv4, strides = [1, stride, stride, 1], padding = 'SAME')
+            c3 = tf.nn.relu(tf.nn.conv2d(c2_pool, filter = W_conv3, strides = [1, stride, stride, 1], padding = 'SAME'))
+            if dropout :
+                c3 = tf.nn.dropout(c3, keep_prob_convs)
+            c4 = tf.nn.relu(tf.nn.conv2d(c3, filter = W_conv4, strides = [1, stride, stride, 1], padding = 'SAME'))
             c4_pool = tf.nn.max_pool(c4, ksize = [1,2,2,1], strides = [1, 2,2,1], padding = 'VALID')
-            c5 = tf.nn.conv2d(c4_pool, filter = W_conv5, strides = [1, stride, stride, 1], padding = 'SAME') #
-            c6 = tf.nn.conv2d(c5, filter = W_conv6, strides = [1, stride, stride, 1], padding = 'SAME')
+            c5 = tf.nn.relu(tf.nn.conv2d(c4_pool, filter = W_conv5, strides = [1, stride, stride, 1], padding = 'SAME'))
+            if dropout :
+                c5 = tf.nn.dropout(c5, keep_prob_convs)
+            c6 = tf.nn.relu(tf.nn.conv2d(c5, filter = W_conv6, strides = [1, stride, stride, 1], padding = 'SAME'))
             c6_pool = tf.nn.max_pool(c6, ksize = [1,2,2,1], strides = [1,2,2,1], padding ='VALID')
-            c7 = tf.nn.conv2d(c6_pool, filter = W_conv7, strides = [1,stride,stride,1], padding = 'SAME')
-            c8 = tf.nn.conv2d(c7, filter = W_conv8, strides = [1, stride, stride, 1], padding = 'SAME')
+            c7 = tf.nn.relu(tf.nn.conv2d(c6_pool, filter = W_conv7, strides = [1,stride,stride,1], padding = 'SAME'))
+            if dropout :
+                c7 = tf.nn.dropout(c7, keep_prob_convs)
+            c8 = tf.nn.relu(tf.nn.conv2d(c7, filter = W_conv8, strides = [1, stride, stride, 1], padding = 'SAME'))
             c8_pool = tf.nn.max_pool(c8, ksize = [1,2,2,1], strides = [1,2,2,1], padding ='VALID')
-            c9 = tf.nn.conv2d(c8_pool, filter = W_conv9, strides = [1,stride,stride,1], padding = 'SAME')
-            c10 = tf.nn.conv2d(c9, filter = W_conv10, strides = [1, stride, stride, 1], padding = 'SAME')
+            c9 = tf.nn.relu(tf.nn.conv2d(c8_pool, filter = W_conv9, strides = [1,stride,stride,1], padding = 'SAME'))
+            if dropout :
+                c9 = tf.nn.dropout(c9, keep_prob_convs)
+            c10 = tf.nn.relu(tf.nn.conv2d(c9, filter = W_conv10, strides = [1, stride, stride, 1], padding = 'SAME'))
             c10_pool = tf.nn.max_pool(c10, ksize = [1,2,2,1], strides = [1,2,2,1], padding ='VALID')
-            c11 = tf.nn.conv2d(c10_pool, filter = W_conv11, strides = [1,stride,stride,1], padding = 'SAME')
-            c12 = tf.nn.conv2d(c11, filter = W_conv12, strides = [1, stride, stride, 1], padding = 'SAME')
+            c11 = tf.nn.relu(tf.nn.conv2d(c10_pool, filter = W_conv11, strides = [1,stride,stride,1], padding = 'SAME'))
+            if dropout :
+                c11 = tf.nn.dropout(c11, keep_prob_convs)
+            c12 = tf.nn.relu(tf.nn.conv2d(c11, filter = W_conv12, strides = [1, stride, stride, 1], padding = 'SAME'))
             c12_pool = tf.nn.max_pool(c12, ksize = [1,2,2,1], strides = [1,2,2,1], padding ='VALID')
         with tf.name_scope('Full_connections') :
             flatten = tf.contrib.layers.flatten(c12_pool)
+            if dropout :
+                flatten = tf.nn.dropout(flatten, keep_prob_hidden)
             fc1 = tf.nn.relu(tf.matmul(flatten, W_fc1) + b_fc1)
+            if dropout :
+                fc1 = tf.nn.dropout(fc1, keep_prob_hidden)
             fc2 = tf.nn.relu(tf.matmul(fc1, W_fc2) +b_fc2)
         with tf.name_scope('Softmax_Classification') :
             logits = tf.matmul(fc2, W_softmax) + b_softmax
