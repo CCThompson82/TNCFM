e79ec7974aa6f18f1a97baf12e7f0d649b77a8a2
Christopher Thompson <christophercthompson82@gmail.com>
2017-02-24 18:42:09 +0000

    feat : Add 3rd layer to autoencoder
    ref: v3.7.0.0

--- AUTOENCODER_GRAPH.py
+++ AUTOENCODER_GRAPH.py
@@ -24,7 +24,23 @@ with graph.as_default() :
             tf.summary.histogram('W_conv2', W_conv2)
             tf.summary.histogram('b_conv2', b_conv2)
 
+            W_conv3 = tf.Variable(
+                        tf.truncated_normal(
+                            [autoencoder_kernels[2], autoencoder_kernels[2], autoencoder_depths[1], autoencoder_depths[2]],
+                            stddev = stddev))
+            b_conv3 = tf.Variable(tf.zeros([autoencoder_depths[2]])) # experiment with this value
+            tf.summary.histogram('W_conv3', W_conv3)
+            tf.summary.histogram('b_conv3', b_conv3)
+
         with tf.variable_scope('Transpose_Convolution') :
+            W_deconv3 = tf.Variable(
+                        tf.truncated_normal(
+                            [autoencoder_kernels[2], autoencoder_kernels[2], autoencoder_depths[1], autoencoder_depths[2]],
+                            stddev = stddev))
+            b_deconv3 = tf.Variable(tf.zeros([autoencoder_depths[1]])) # experiment with this value
+            tf.summary.histogram('W_deconv3', W_deconv3)
+            tf.summary.histogram('b_deconv3', b_deconv3)
+
             W_deconv2 = tf.Variable(
                         tf.truncated_normal(
                             [autoencoder_kernels[1], autoencoder_kernels[1], autoencoder_depths[0], autoencoder_depths[1]],
@@ -36,7 +52,7 @@ with graph.as_default() :
 
             W_deconv1 = tf.Variable(
                             tf.truncated_normal(
-                                [autoencoder_kernels[1], autoencoder_kernels[1], num_channels, autoencoder_depths[0]],
+                                [autoencoder_kernels[0], autoencoder_kernels[0], num_channels, autoencoder_depths[0]],
                                 stddev = stddev))
             b_deconv1 = tf.Variable(tf.zeros([num_channels]))
             tf.summary.histogram('W_deconv1', W_deconv1)
@@ -54,12 +70,26 @@ with graph.as_default() :
                                 filter = W_conv2,
                                 strides = [1, autoencoder_strides[1], autoencoder_strides[1], 1],
                                 padding = 'SAME') + b_conv2)
-        return e1, e2
+
+            e3 = tf.nn.relu(
+                    tf.nn.conv2d(e2,
+                                filter = W_conv3,
+                                strides = [1, autoencoder_strides[2], autoencoder_strides[2], 1],
+                                padding = 'SAME') + b_conv3)
+        return e1, e2, e3
 
     def decoder(data) :
         with tf.name_scope('Decoder') :
-            d2 = tf.nn.conv2d_transpose(
+            d3 = tf.nn.conv2d_transpose(
                     data,
+                    filter = W_deconv3,
+                    output_shape = [batch_size, fovea_size // autoencoder_strides[2] // autoencoder_strides[1], fovea_size // autoencoder_strides[2] // autoencoder_strides[1], autoencoder_depths[1]],
+                    strides = [1, autoencoder_strides[2], autoencoder_strides[2], 1],
+                    padding = 'SAME') + b_deconv3
+
+
+            d2 = tf.nn.conv2d_transpose(
+                    d3,
                     filter = W_deconv2,
                     output_shape = [batch_size, fovea_size // autoencoder_strides[1], fovea_size // autoencoder_strides[1], autoencoder_depths[0]],
                     strides = [1, autoencoder_strides[1], autoencoder_strides[1], 1],
@@ -73,16 +103,16 @@ with graph.as_default() :
                         strides = [1, autoencoder_strides[0], autoencoder_strides[0], 1],
                         padding = 'SAME')
                     + b_deconv1)
-        return d1, d2
+        return d1, d2, d3
 
     with tf.name_scope('Input_fovea') :
         alpha = tf.placeholder(tf.float32, shape = ())
         batch_fovea = tf.placeholder(tf.float32, shape = [batch_size, fovea_size, fovea_size, num_channels])
 
     with tf.name_scope('Encoding') :
-        e1, encoded = encoder(batch_fovea)
+        e1, e2, encoded = encoder(batch_fovea)
     with tf.name_scope('Decoding') :
-        decoded, d2 = decoder(encoded)
+        decoded, d2, d3 = decoder(encoded)
 
 
     with tf.name_scope('Training') :
@@ -91,8 +121,11 @@ with graph.as_default() :
         train_op = tf.train.GradientDescentOptimizer(alpha).minimize(total_loss)
 
     with tf.name_scope('Summaries') :
-        tf.summary.histogram('encoded_final', encoded)
         tf.summary.histogram('encoded_layer1', e1)
+        tf.summary.histogram('encoded_layer2', e2)
+        tf.summary.histogram('encoded_final', encoded)
+
+
         tf.summary.scalar('log_loss', logloss)
         tf.summary.scalar('learning_rate', alpha)
         summaries = tf.summary.merge_all()
